a0c737fdffe436652ad046b5a314783d5efc2f53
M J <melvynu19@gmail.com>
2024-07-05 23:18:40 +0200

    dunno felt like removing some comments seemed a lil much

--- functions.py
+++ functions.py
@@ -148,4 +148,3 @@ def set_alarm():
     })
 
     return jsonify({"status": "Alarm set", "time": alarm_time_str, "message": alarm_message})
-# still working to fix some bugs also for the alarmyou need to declare it in your json since at this point there isn't really a database structure
--- weather_test_copy_chatbot.py
+++ weather_test_copy_chatbot.py
@@ -5,19 +5,14 @@ import asyncio
 import os
 
 async def getweather():
-  # declare the client. the measuring unit used defaults to the metric system (celcius, km/h, etc.)
   async with python_weather.Client(unit=python_weather.METRIC) as client:
-    # fetch a weather forecast from a city
     weather = await client.get('Amsterdam')
-    
-    # returns the current day's forecast temperature (int)
+
     print(weather.temperature)
     
-    # get the weather forecast for a few days
     for daily in weather.daily_forecasts:
       print(daily)
-      
-      # hourly forecasts
+
       for hourly in daily.hourly_forecasts:
         print(f' --> {hourly!r}')
 
